c36c6a96faa70f9d406096f83cf0a0576862923f
phuc1208 <vinhphucit02@gmail.com>
2023-10-23 21:35:59 +0700

    Test

--- src/functions/email-puller.ts
+++ src/functions/email-puller.ts
@@ -1,7 +1,18 @@
 
 import type { Config } from "@netlify/functions"
+import fastify from "../bootstrap";
+import axios from "axios";
 
 export default async (req: Request) => {
+    const url = `https://gmail.googleapis.com/gmail/v1/users/me/profile`;
+    const { token } = await fastify.oAuthClient.getAccessToken();
+    console.log("token", token);
+    const response = await axios.get(url, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
+    console.log("res ", response);
     const { next_run } = await req.json()
     console.log(next_run);
     console.log("Received event! Next invocation at:", next_run)
