2bac93baf62d53555e694adab67be169ea0385fc
phuc1208 <vinhphucit02@gmail.com>
2023-10-23 22:05:02 +0700

    test

--- src/functions/email-puller.ts
+++ src/functions/email-puller.ts
@@ -4,6 +4,7 @@ import fastify from "../bootstrap";
 import axios from "axios";
 
 const handler: Handler = schedule('* * * * *', async () => {
+    await fastify.ready();
     const url = `https://gmail.googleapis.com/gmail/v1/users/me/profile`;
     const { token } = await fastify.oAuthClient.getAccessToken();
     console.log("token", token);
